c2c85e7fceab7125771e80db17d5c2b992ed9627
tsubasanagatsuka <reach.283.0921@gmail.com>
2022-06-09 16:53:51 +0900

    コメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @items = Item.includes(:user).order("created_at DESC")
+    # @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
