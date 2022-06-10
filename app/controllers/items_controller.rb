e36277da91db22b49a3aa9386313f249269c8596
tsubasanagatsuka <reach.283.0921@gmail.com>
2022-06-10 22:34:16 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,23 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if @item.user_id == current_user.id && @item.purchase.nil?
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+       redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :state_id, :delivery_burden_id, :prefecture_id, :ship_day_id, :price).merge(user_id: current_user.id)
